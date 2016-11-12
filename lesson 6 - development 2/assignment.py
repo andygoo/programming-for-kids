75cb8312109240a1be1d357ae5370b67cb984e25
Matt Yancey <matthewyancey@gmail.com>
2016-11-12 10:25:17 -0500

    New lesson

---
+++ lesson 6 - development 2/assignment.py
@@ -0,0 +1,22 @@
+# Problem 1 - If you take the numbers from 0 to 10 and sum seperatly the odds
+# and evens you get you 25 for the odd and 30 for the even.
+# find and print out the sums of odd and even numbers from 0 to 100.
+
+
+# Problem 2 - write a program that asks a user for their name and age.
+# Then have it print out "hello <name>, you're really young" if their age is
+# 15 or less, and "hello <name>, you're really experenced" if they are older.
+
+
+# Problem 3 - If we list all the natural numbers below 10 that are multiples
+# of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+
+# Problem 4 - Take the list [8, 4, 39, 6 ,10, 2, 49] and write a program that
+# will print the list but sorted in accending order.
+
+
+# Problem 5 - Make a program that will ask you for a three characters (e.g., XLF)
+# then have it loop through all the combinations until it finds yours.
+# have it print out each guess.
